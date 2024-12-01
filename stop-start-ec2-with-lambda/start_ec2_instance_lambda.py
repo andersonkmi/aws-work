da77e7c0b3fa41f855409447b0eb4ed57cc8ad62
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-12-01 10:01:46 +0100

    Changes to wait for instance to run and associate IP address

--- stop-start-ec2-with-lambda/start_ec2_instance_lambda.py
+++ stop-start-ec2-with-lambda/start_ec2_instance_lambda.py
@@ -10,6 +10,7 @@ def lambda_handler(event, context):
     # Default region
     region_name = os.environ['region']
     instance_id = os.environ['instance_id']
+    allocation_id = os.environ['allocation_id']
     
     if not instance_id:
         logger.error("No instance_id provided in the event.")
@@ -21,18 +22,25 @@ def lambda_handler(event, context):
     try:
         # Create EC2 client
         ec2_client = boto3.client('ec2', region_name=region_name)
+        ec2_waiter = ec2_client.get_waiter('instance_running')
         
         # Start the EC2 instance
         response = ec2_client.start_instances(InstanceIds=[instance_id])
-        
+
+        # Wait until the instance is running
+        logger.info(f"Waiting for instance {instance_id} to enter 'running' state.")
+        ec2_waiter.wait(InstanceIds=[instance_id])
+
         # Extract and log instance state
-        starting_instance = response['StartingInstances'][0]
-        current_state = starting_instance['CurrentState']['Name']
-        logger.info(f"Instance {instance_id} is now {current_state}.")
-        
+        logger.info(f"Instance {instance_id} is now running.")
+
+        # Associate the EIP
+        eip_association_response = ec2_client.associate_address(InstanceId=instance_id, AllocationId=allocation_id, AllowReassociation=True)
+        print(f"Elastic IP associated successfully: {eip_association_response['AssociationId']}")
+
         return {
             "statusCode": 200,
-            "body": f"Instance {instance_id} is now {current_state}."
+            "body": f"Instance {instance_id} is now running."
         }
     
     except Exception as e:
